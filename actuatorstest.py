987c23821e5f8d6aeb21d51b92a5f9be4c609d1e
alliwang73 <alliwang@berkeley.edu>
2025-04-15 11:18:48 -0700

    Update and rename actuatorstesting.py to actuatorstest.py

--- actuatorstest.py
+++ actuatorstest.py
@@ -21,34 +21,6 @@ while True:
     set_angle(new_angle)
     time.sleep(30)  # Wait 30 seconds before next move
 
-#speaker
-led_ext = Pin(27, mode=Pin.OUT)
-while True:
-    new_frequency = random.randint(0, 180)
-    L1 = PWM(led_ext,freq=new_frequency,duty=100)
-    time.sleep(30)  # Wait 30 seconds before next move
-#duty_cycle = 100
-#L1 = PWM(led_ext,freq=500,duty=duty_cycle)
-# various notes and pitches
-C4 = 262
-
-# This example is bach prelude in C; you can try different tunes by changing
-# the notes here.
-music = [
-C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,C4,
-]
-note_index = 0
-# This is the callback function
-def tcb(timer):
-  global duty_cycle
-  global note_index
-  if note_index < len(music)-1:
-    note_index += 1
-  else:
-    note_index = 0
-    L1.freq(music[note_index])
-    t1 = Timer(1)
-
 
 # Callback means that everytime the timer
 # counts one, the corresponding function (in this case tcb)
